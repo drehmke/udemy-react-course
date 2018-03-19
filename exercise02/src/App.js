6b91ce252b1ffa12feaa400ed34b2f2dfaa768e7
Donna Ryan <donna@awdd.net>
2018-03-19 08:55:48 -0700

    Exercise 02 - starting to work with the CharComponent

--- exercise02/src/App.js
+++ exercise02/src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
           <li className="done">Create an input field (in App Component) with a change listener which outputs the length of the entered text below it. (e.g. in a paragraph)</li>
           <li className="done">Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
           <li className="done">Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length. (e.g. take 5 as a minimum length)</li>
-          <li>Create another component (=> CharComponent) and style it as an inline box (display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black)</li>
+          <li className="done">Create another component (=> CharComponent) and style it as an inline box (display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black)</li>
           <li>Render a list of CharComponents where each component receives a different letter of the entered text (in the initial input field) as a prop</li>
           <li>When you click a CharComponent, the character it holds should be removed from the entered text</li>
         </ol>
---
+++ exercise02/src/Components/Char/Char.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './char.css'
+
+const CharComponent = (props) => {
+  return (
+    <div className="charContainer">
+      <div className="charElem"></div>
+    </div>
+  )
+}
+
+export default CharComponent
