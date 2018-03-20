fc50eb2b34d557a8dcdec88b248700f3afdb2113
Donna Ryan <donna@awdd.net>
2018-03-20 16:38:48 -0700

    Section 7 Lection 99 - PropTypes

--- react-001/src/Components/Persons/Person/Person.js
+++ react-001/src/Components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import InvisiWrapper from '../../../Hoc/InvisiWrapper'
 import componentClass from '../../../Hoc/componentClass'
 import styles from './person.css'
@@ -26,5 +27,12 @@ class Person extends Component {
       </InvisiWrapper>
     )
   }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 }
 export default componentClass(Person, styles.Person)
