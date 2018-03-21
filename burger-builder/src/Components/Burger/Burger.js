88ff7eb07f104e5ffbbe708b150fc5562606a9de
Donna Ryan <donna@awdd.net>
2018-03-21 10:20:28 -0700

    Burger Builder - transformed burger ingredients object

--- burger-builder/src/Components/Burger/Burger.js
+++ burger-builder/src/Components/Burger/Burger.js
@@ -3,13 +3,18 @@ import Ingredients from './Ingredients'
 import styles  from '../Styles/Burger.css'
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])]
+        .map((_, i) => {
+          return <Ingredients key={igKey + i} type={igKey} />
+        })
+    })
+
   return(
     <div className={styles.burger}>
       <Ingredients type="bread-top" />
-      <Ingredients type="lettuce" />
-      <Ingredients type="bacon" />
-      <Ingredients type="cheese" />
-      <Ingredients type="meat" />
+      {transformedIngredients}
       <Ingredients type="bread-bottom" />
     </div>
 
--- burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import InvisiWrapper from '../../Hoc/InvisiWrapper'
 import Burger from '../../Components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      lettuce: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
+
   render() {
     return(
       <InvisiWrapper>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </InvisiWrapper>
     )
