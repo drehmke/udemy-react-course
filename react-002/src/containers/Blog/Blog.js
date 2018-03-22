15d2991efc9f5664d962cbed853dfd90b4a09907
Donna Ryan <donna@awdd.net>
2018-03-22 15:24:45 -0700

    Section 9 Lectures 144-151 - fetching data without inifinite loops

--- react-002/src/containers/Blog/Blog.js
+++ react-002/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,53 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+    selectedPostId: null
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then( (response) => {
+        // console.log(response)
+        const posts = response.data.slice(0,4)
+        const updatedPosts = posts.map( (post) => {
+          return {
+            ...post,
+            author: 'Donna'
+          }
+        })
+        this.setState({posts: updatedPosts })
+      })
+  }
+
+  getSinglePostHandler = (id) => {
+    this.setState({selectedPostId: id})
+  }
+
+  render () {
+    const posts = this.state.posts.map( (post) => {
+      return <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={() => this.getSinglePostHandler(post.id)}
+       />
+    })
+      return (
+          <div>
+              <section className="Posts">
+                  {posts}
+              </section>
+              <section>
+                  <FullPost id={this.state.selectedPostId} />
+              </section>
+              <section>
+                  <NewPost />
+              </section>
+          </div>
+      );
+  }
 }
 
 export default Blog;
