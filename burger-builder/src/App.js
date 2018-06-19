a0a8aed1ab0fb2075a1f07804fe15fef438dc0fc
Donna Ryan <dryan@prizelogic.com>
2018-06-19 08:46:11 -0700

    Protecting routes from unauthenticated users

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom'
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import Layout from './Components/Layout/Layout'
@@ -24,22 +24,35 @@ class App extends Component {
   }
 
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" component={BurgerBuilder} exact />
+        <Redirect to="/" />
+      </Switch>
+    )
+    if( this.props.isAuthenticated ) {
+      routes = (
+        <Switch>
+          <Route path="/orderhistory" component={Orders} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path ="/logout" component={Logout} />
+          <Route path="/" component={BurgerBuilder} exact />
+        </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/orderhistory" component={Orders} />
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/auth" component={Auth} />
-            <Route path ="/logout" component={Logout} />
-            <Route path="/" component={BurgerBuilder} exact />
-          </Switch>
-
-
+          {routes}
         </Layout>
       </div>
     );
   }
+}
+
+const mapStateToProps = state => {
+  isAuthenticated: state.auth.token !== null
 }
 
 const mapDispatchToProps = dispatch => {
@@ -48,4 +61,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
